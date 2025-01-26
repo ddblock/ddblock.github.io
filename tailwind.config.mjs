d8bda3429e6a4a220b4f2bbc4030be5720463c11
Karl <adler@fudler.de>
2025-01-26 15:02:11 +0100

    add typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,7 +12,48 @@ export default {
 					'dark': '#1A202C',      // Very dark blue-gray
 				},
 			},
+			typography: {
+				DEFAULT: {
+					css: {
+						'h1, h2, h3, h4': {
+							color: '#2D3748',
+							fontWeight: '700',
+						},
+						a: {
+							color: '#4299E1',
+							textDecoration: 'none',
+							'&:hover': {
+								color: '#2D3748',
+								textDecoration: 'underline',
+							},
+						},
+						ul: {
+							listStyleType: 'disc',
+						},
+						ol: {
+							listStyleType: 'decimal',
+						},
+						'ul, ol': {
+							paddingLeft: '1.5em',
+						},
+						'li': {
+							marginTop: '0.5em',
+							marginBottom: '0.5em',
+						},
+						blockquote: {
+							borderLeftColor: '#4299E1',
+							color: '#4A5568',
+						},
+						code: {
+							color: '#2D3748',
+							backgroundColor: '#EDF2F7',
+							padding: '0.2em 0.4em',
+							borderRadius: '0.25em',
+						},
+					},
+				},
+			},
 		},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography')],
 } 
