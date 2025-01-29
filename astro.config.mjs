dbb4f391161d60aa20ee414ebab50050c086eaec
Karl <adler@fudler.de>
2025-01-29 20:47:06 +0100

    indentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blockchain-dresden.de',
-	integrations: [mdx(), sitemap(), tailwind()],
+  site: 'https://blockchain-dresden.de',
+  integrations: [mdx(), sitemap(), tailwind()],
 });
